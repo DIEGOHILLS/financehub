6cc105a91ca22f3115c070507965463e9ff59d01
DIEGOHILLS <hillsdiego33@gmail.com>
2025-12-11 23:59:38 +0200

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   
   resolve: {
     alias: {
