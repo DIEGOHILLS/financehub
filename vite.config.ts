32869ff76c6d2556616fc015d0012842577029e2
DIEGOHILLS <hillsdiego33@gmail.com>
2025-12-11 23:42:27 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,34 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  root: process.cwd(),
+  
   server: {
     host: "::",
     port: 8080,
   },
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  
+  
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      
+      input: {
+        main: path.resolve(__dirname, "index.html")
+      }
+    }
+  },
+
+  publicDir: "public",
 }));
